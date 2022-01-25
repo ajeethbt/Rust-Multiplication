1faa0d6b84af0b856cb019e65da276fa6ba29977
Ajeeth B.T <ajeeth.bt@gmail.com>
2022-01-25 10:45:46 +0530

    Code Optimization Update

--- src/main.rs
+++ src/main.rs
@@ -2,100 +2,22 @@ fn main() {
     let mut num = 1;
     let mut ans = 0;
     let mut num1 = 1;
-    const num2:i32 = 4;
+    let mut times = 1;
 
     println!("Made using Rust by Ajeeth");
     println!("What does it do? \nIts simple loop test multiplication.");
-    
+    while times < 11 {
     while num < 11 {
         ans = num1 * num;
-        println!("1 * {} = {}", num,ans);
+        println!("{} * {} = {}", times,num,ans);
         num = num + 1;
        
     }
     print!("\n");
     num1 = num1 + 1;
     num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("2 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("3 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("4 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("5 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("6 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("7 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("8 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("9 * {} = {}", num,ans);
-        num = num + 1;
-    }
-    print!("\n");
-    num1 = num1 + 1;
-    num = 1;
-
-    while num < 11 {
-        ans = num1 * num;
-        println!("10 * {} = {}", num,ans);
-        num = num + 1;
+    times = times + 1;
     }
 
-   println!("The code is not fully complete since its not optimized");
-   println!("It could have been done in a better way using for loops")
     }
     
